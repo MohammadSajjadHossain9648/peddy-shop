6e5de925399b1930aadf2bbb11cf2882a94e5e42
mohammadsajjadhossain9648 <sajjadhossain9648@gmail.com>
2024-10-06 22:40:00 +0600

    Add responsive footer section using HTML and daisyUI

--- tailwind_custom_configuration.js
+++ tailwind_custom_configuration.js
@@ -5,6 +5,7 @@ tailwind.config = {
           white_color: 'rgb(255, 255, 255)',
           footer_white_color1: 'rgba(255, 255, 255, 0.9)',
           footer_white_color2: 'rgba(255, 255, 255, 0.6)',
+          input_box_bg: 'rgba(255, 255, 255, 0.05)',
           green_color: 'rgb(14, 122, 129)',
           black_color: 'rgb(19, 19, 19)',
           shade_black_color1: 'rgba(19, 19, 19, 0.7)',
