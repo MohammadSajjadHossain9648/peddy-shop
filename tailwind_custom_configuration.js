9b311397581ca312aec6f500e9ca93c4b9d68fc8
mohammadsajjadhossain9648 <sajjadhossain9648@gmail.com>
2024-10-07 21:21:48 +0600

    Add responsive adopt section using HTML and daisyUI

--- tailwind_custom_configuration.js
+++ tailwind_custom_configuration.js
@@ -1,16 +1,22 @@
 tailwind.config = {
-    theme: {
-      extend: {
-        colors: {
-          white_color: 'rgb(255, 255, 255)',
-          footer_white_color1: 'rgba(255, 255, 255, 0.9)',
-          footer_white_color2: 'rgba(255, 255, 255, 0.6)',
-          input_box_bg: 'rgba(255, 255, 255, 0.05)',
-          green_color: 'rgb(14, 122, 129)',
-          black_color: 'rgb(19, 19, 19)',
-          shade_black_color1: 'rgba(19, 19, 19, 0.7)',
-          shade_black_color2: 'rgba(19, 19, 19, 0.6)',
-        }
+  theme: {
+    extend: {
+      colors: {
+        white_color: 'rgb(255, 255, 255)',
+        white_footer_color1: 'rgba(255, 255, 255, 0.9)',
+        white_footer_color2: 'rgba(255, 255, 255, 0.6)',
+        input_box_bg: 'rgba(255, 255, 255, 0.05)',
+
+        green_color: 'rgb(14, 122, 129)',
+        green_border_color: 'rgba(14, 122, 129, 0.15)',
+        green_bg_color: 'rgba(14, 122, 129, 0.1)',
+
+        black_color: 'rgb(19, 19, 19)',
+        shade_black_color1: 'rgba(19, 19, 19, 0.7)',
+        shade_black_color2: 'rgba(19, 19, 19, 0.6)',
+        black_border_color: 'rgba(19, 19, 19, 0.1)',
+        black_bg_color: 'rgba(19, 19, 19, 0.03)',
       }
     }
   }
+}
